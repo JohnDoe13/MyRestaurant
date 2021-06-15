1605987ed322759cbaf5aa422566f7b249f6e0d1
JohnDoe13 <nick.alzen@live.com>
2021-06-15 16:43:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MyRestaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Staff mystaff = new Staff();
+            mystaff.FullStaff();
+
+            
+        //Egentligen så hade jag velat ha denna kod som sedan i methoden WholeStff() på något sätt lopade runt
+        //antal anställda och lön. Nåväl, provade lite fram o tillbaks. 
+
+         //Svar 1: De anställda o säkert löner också men jag valde att baka ihop dem.
+         //Svar 2. Ville egentligen bara ha 1st method som skrev ut alla anställda. Men men...
+
+            //Console.WriteLine("Number of personnel?");
+            //howmany = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < howmany; i++)
+            //{
+            //    Console.WriteLine(("Type the name of your staff:"));
+            //    mystaff.name = Console.ReadLine();
+
+            //    Console.WriteLine("Type salary:");
+            //    mystaff.salary = int.Parse(Console.ReadLine());
+
+            //}
+
+            mystaff.WholeStaff();
+            
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyRestaurant
+{
+    class Staff
+    {
+        public string name;
+        public int salary;
+        int howmany;
+        public void FullStaff()
+        {
+
+            Console.WriteLine("Number of personnel?");
+            howmany = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < howmany; i++)
+            {
+                Console.WriteLine(("Type the name of your staff:"));
+                name = Console.ReadLine();
+
+                Console.WriteLine("Type salary:");
+                salary = int.Parse(Console.ReadLine());
+
+            }
+
+
+        }
+
+        public void WholeStaff()
+             {
+                    for (int i=0;i<howmany; i++)
+                    Console.WriteLine("Name of your staff {0} and salary {1}", name, salary);
+            }
+        
+
+        
+    }
+}
+
+        
+   
+            
+
+    
+
+  
